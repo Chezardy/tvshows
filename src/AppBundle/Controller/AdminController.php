c508fdb9bbd135407b21079e6f86251984b8ff03
Chezardy <arno33370@hotmail.fr>
2017-01-21 15:04:28 +0100

    added deleting entire seasons

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -79,8 +79,15 @@ class AdminController extends Controller
     public function deleteSeasonAction($id)
     {
         $em = $this->get('doctrine')->getManager();
-        $repo = $em->getRepository('AppBundle:TVShow');
-        // if ($)
+        $repo = $em->getRepository('AppBundle:Season');
+        if ($season = $repo->find($id)) {
+            $id = $season->getShow()->getId();
+            $em->remove($season);
+            $em->flush();
+            return $this->redirect($this->generateUrl('show', ['id' => $id]));
+        } else {
+            return $this->redirect($this->generateUrl('homepage'));
+        }
     }
 
     /**
--- src/AppBundle/Entity/Season.php
+++ src/AppBundle/Entity/Season.php
@@ -29,7 +29,7 @@ class Season
     private $number;
 
     /**
-     * @ORM\OneToMany(targetEntity="Episode", mappedBy="season")
+     * @ORM\OneToMany(targetEntity="Episode", mappedBy="season", orphanRemoval=true)
      * @ORM\OrderBy({"number" = "ASC"})
      */
     private $episodes;
